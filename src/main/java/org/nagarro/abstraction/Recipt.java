41bc7961cdc19daae5e9c46d8180a5797c1675c0
rolger <roland.germ@gmail.com>
2021-08-30 15:43:36 +0200

    refactor: applied Java formating rules

--- src/main/java/org/nagarro/abstraction/Recipt.java
+++ src/main/java/org/nagarro/abstraction/Recipt.java
@@ -2,23 +2,23 @@ package org.nagarro.abstraction;
 
 public class Recipt {
 
-	public String build(int number) {
+    public String build(int number) {
 
-		switch (number) {
-		case 4:
-			return "Awesome Pancakes" //
-					+ "\n\n" //
-					+ "In a large bowl, mix 1 cup all-purpose flour, 2 tablespoons white sugar, 2 teaspoons baking powder and 1 teaspoon salt. Make a well in the center, and pour in 1 cup milk, 1 egg and 2 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. This yields in 4 servings. Brown on both sides and serve hot.";
+	switch (number) {
+	case 4:
+	    return "Awesome Pancakes" //
+		    + "\n\n" //
+		    + "In a large bowl, mix 1 cup all-purpose flour, 2 tablespoons white sugar, 2 teaspoons baking powder and 1 teaspoon salt. Make a well in the center, and pour in 1 cup milk, 1 egg and 2 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. This yields in 4 servings. Brown on both sides and serve hot.";
 
-		case 8:
+	case 8:
 
-			return "Awesome Pancakes" //
-					+ "\n\n" //
-					+ "In a large bowl, mix 2 cup all-purpose flour, 4 tablespoons white sugar, 4 teaspoons baking powder and 2 teaspoon salt. Make a well in the center, and pour in 2 cup milk, 2 egg and 4 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/8 cup for each pancake. This yields in 8 servings. Brown on both sides and serve hot.";
+	    return "Awesome Pancakes" //
+		    + "\n\n" //
+		    + "In a large bowl, mix 2 cup all-purpose flour, 4 tablespoons white sugar, 4 teaspoons baking powder and 2 teaspoon salt. Make a well in the center, and pour in 2 cup milk, 2 egg and 4 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/8 cup for each pancake. This yields in 8 servings. Brown on both sides and serve hot.";
 
-		default:
-			return "";
-		}
-
+	default:
+	    return "";
 	}
+
+    }
 }
--- src/test/java/org/nagarro/abstraction/ReciptTest.java
+++ src/test/java/org/nagarro/abstraction/ReciptTest.java
@@ -10,29 +10,24 @@ class ReciptTest {
 
     @Test
     void test0() {
-    	var result = subject.build(0);
-        
-    	assertThat(result).isEmpty();
-    }
-    
-    @Test
-    void test1() {
-    	var result = subject.build(4);
-        
-    	assertThat(result).isEqualTo("Awesome Pancakes"
-    			+ "\n\n"
-    			+ "In a large bowl, mix 1 cup all-purpose flour, 2 tablespoons white sugar, 2 teaspoons baking powder and 1 teaspoon salt. Make a well in the center, and pour in 1 cup milk, 1 egg and 2 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. This yields in 4 servings. Brown on both sides and serve hot."
-    			);
+	var result = subject.build(0);
+
+	assertThat(result).isEmpty();
     }
 
+    @Test
+    void test1() {
+	var result = subject.build(4);
+
+	assertThat(result).isEqualTo("Awesome Pancakes" + "\n\n"
+		+ "In a large bowl, mix 1 cup all-purpose flour, 2 tablespoons white sugar, 2 teaspoons baking powder and 1 teaspoon salt. Make a well in the center, and pour in 1 cup milk, 1 egg and 2 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. This yields in 4 servings. Brown on both sides and serve hot.");
+    }
 
     @Test
     void test2() {
-    	var result = subject.build(8);
-        
-    	assertThat(result).isEqualTo("Awesome Pancakes"
-    			+ "\n\n"
-    			+ "In a large bowl, mix 2 cup all-purpose flour, 4 tablespoons white sugar, 4 teaspoons baking powder and 2 teaspoon salt. Make a well in the center, and pour in 2 cup milk, 2 egg and 4 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/8 cup for each pancake. This yields in 8 servings. Brown on both sides and serve hot."
-    			);
+	var result = subject.build(8);
+
+	assertThat(result).isEqualTo("Awesome Pancakes" + "\n\n"
+		+ "In a large bowl, mix 2 cup all-purpose flour, 4 tablespoons white sugar, 4 teaspoons baking powder and 2 teaspoon salt. Make a well in the center, and pour in 2 cup milk, 2 egg and 4 tablespoons vegetable oil. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/8 cup for each pancake. This yields in 8 servings. Brown on both sides and serve hot.");
     }
 }
